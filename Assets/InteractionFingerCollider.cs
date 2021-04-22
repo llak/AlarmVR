eeaad9d25f129830ab7a9fc25f54a7527e8644f9
basho <basho@umich.edu>
2021-04-22 18:06:44 -0400

    please

---
+++ Assets/InteractionFingerCollider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InteractionFingerCollider : MonoBehaviour
+{
+
+    public GameObject sliderController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerHandLeft" || other.gameObject.tag == "PlayerHandRight")
+        {
+            //sliderController.BroadcastMessage("FingerOnTriggerEnter", other);
+        }
+
+        sliderController.BroadcastMessage("FingerOnTriggerEnter", other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerHandLeft" || other.gameObject.tag == "PlayerHandRight")
+        {
+            //sliderController.BroadcastMessage("FingerOnTriggerExit", other);
+        }
+        sliderController.BroadcastMessage("FingerOnTriggerExit", other);
+    }
+}
---
+++ Assets/TestTracking.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using OculusSampleFramework;
+
+public class TestTracking : MonoBehaviour
+{
+    public InteractableToolsCreator handTools;
+    private GameObject fingerTip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fingerTip = FindObjectOfType<FingerTipPokeTool>().gameObject;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = fingerTip.transform.position;
+
+    }
+}
